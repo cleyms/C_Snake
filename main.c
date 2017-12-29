f5c923a9f7dc6c0b0e7a7a956f16000fd872e02f
Clement Def <33748126+cleyms@users.noreply.github.com>
2017-12-29 13:21:23 +0100

    FPS bug

--- main.c
+++ main.c
@@ -11,26 +11,27 @@ int main(int argc, char *argv[]){
 	SDL_Event event;
 	int close = 0, i;
 	while(!close){
-		SDL_PollEvent(&event);
-		if(event.type == SDL_QUIT){
-			close = 1;
-		}else if(event.type == SDL_KEYDOWN){
-			switch(event.key.keysym.sym){
-				case SDLK_ESCAPE:
-					close = 1;
-					break;
-				case SDLK_RIGHT:
-					player.dir = 0;
-					break;
-				case SDLK_UP:
-					player.dir = 1;
-					break;
-				case SDLK_LEFT:
-					player.dir = 2;
-					break;
-				case SDLK_DOWN:
-					player.dir = 3;
-					break;
+		while(SDL_PollEvent(&event)){
+			if(event.type == SDL_QUIT){
+				close = 1;
+			}else if(event.type == SDL_KEYDOWN){
+				switch(event.key.keysym.sym){
+					case SDLK_ESCAPE:
+						close = 1;
+						break;
+					case SDLK_RIGHT:
+						player.dir = 0;
+						break;
+					case SDLK_UP:
+						player.dir = 1;
+						break;
+					case SDLK_LEFT:
+						player.dir = 2;
+						break;
+					case SDLK_DOWN:
+						player.dir = 3;
+						break;
+				}
 			}
 		}
 		updateSnake(&player);
@@ -39,7 +40,7 @@ int main(int argc, char *argv[]){
 		setColor(win, 0x640000);
 		drawSnake(win, player);
 		SDL_Flip(win->screen);
-		SDL_Delay(200);
+		SDL_Delay(100);
 	}
 	destroySnake(player);
 	endSDL(win);
